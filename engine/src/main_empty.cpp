4c32ce15b53e1490f0e3695e2e7de8d07c5ae919
jesrnSU <119290721+jesrnSU@users.noreply.github.com>
2024-01-06 18:10:00 +0100

    Main will now load every asset from the resource directory automatically

--- engine/src/main_empty.cpp
+++ engine/src/main_empty.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include "GameEngine.h"
 #include <vector>
+#include <filesystem>
+
+using dir_iterator = std::filesystem::recursive_directory_iterator;
 
 
 int main(int argc, char* argv[]) {
@@ -9,11 +12,19 @@ int main(int argc, char* argv[]) {
 	std::cout << s1 << std::endl;
 	
 
+	
 	SYSTEM.initSDLComponents();
 
 	GameEngine* game = GameEngine::get_instance();
 
-	std::vector<std::string> assets = {"resources/images/bg.jpg", "resources/sounds/TillSpel.mp3"};
+	std::vector<std::string> assets;
+	game->load_assets(assets);
+
+	for(const auto& dirEntry : dir_iterator("resources")){
+		if(dirEntry.is_regular_file())
+			assets.push_back(dirEntry.path().generic_string());
+	}
+
 	game->load_assets(assets);
 
 	Sprite* s = new Sprite("resources/images/bg.jpg", 50,50,100,100);
@@ -21,7 +32,6 @@ int main(int argc, char* argv[]) {
 	
 	game->add_sprite(*s);
 	game->add_sprite(*s2);
-	
 
 	game->run_game();
 	
