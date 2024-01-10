46b2f54c510b9d2a3ebb564b29350bc6ef8dac73
Alfred <alfred.berggren@gmail.com>
2024-01-10 12:47:31 +0100

    check before changing texture

--- engine/include/NPC.h
+++ engine/include/NPC.h
@@ -13,6 +13,10 @@ class NPC : public Character {
     // Flyttar NPC:n mot närmsta sprite som är mindre än sig själv
     void move_to_closest();
     void mouseMoved(double x, double y);
+    enum class State { NERVOUS, HUNGRY, ANGRY };
+    State state;
+    // Radie runt NPC:n där den kan se mat
+    static const int FOG_RADIUS = 250;
 
    private:
 };
