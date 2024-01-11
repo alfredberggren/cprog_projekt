ca4b3c90431468ed3f7c5224ef1653a2c4e3f38d
Alfred <alfred.berggren@gmail.com>
2024-01-11 17:02:14 +0100

    comment

--- engine/src/GlobuleGobbleSprite.cpp
+++ engine/src/GlobuleGobbleSprite.cpp
@@ -1,5 +1,24 @@
 #include "GlobuleGobbleSprite.h"
 
+//CAMERA ZOOM
+/* int GlobuleGobbleSprite::get_rendered_h() const {
+    if (Camera::get_instance()->get_focused_on() == this) {
+        return 100;  // bör vara variabel
+    } else {
+        return 100 +
+               (getH() - Camera::get_instance()->get_focused_on()->getH());
+    }
+}
+
+int GlobuleGobbleSprite::get_rendered_w() const {
+    if (Camera::get_instance()->get_focused_on() == this) {
+        return 100;  // bör vara variabel
+    } else {
+        return 100 +
+               (getW() - Camera::get_instance()->get_focused_on()->getW());
+    }
+} */
+
 int GlobuleGobbleSprite::get_rendered_x() const {
     return get_rect()->x - Camera::get_instance()->get_x();
 }
